fca18a39ff84abfe8566e4c22f5f911756c909b5
keegan <kelvinkeegen17@gmail.com>
2025-09-01 17:30:11 +0000

    moddd entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-exec code-server --bind-addr 0.0.0.0:8080 . \
-  ${PROXY_DOMAIN:+--proxy-domain "${PROXY_DOMAIN}"}
+# exec code-server --bind-addr 0.0.0.0:8080 . \
+#   ${PROXY_DOMAIN:+--proxy-domain "${PROXY_DOMAIN}"}
+
+exec code-server --bind-addr 0.0.0.0:9000 --auth none .
